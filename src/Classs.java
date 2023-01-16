a0ecb4346011201e50c0d3c27539f09415089b87
Abdibaitov <91374399+Abdibaitov@users.noreply.github.com>
2023-01-16 18:33:18 +0300

    Initial commit

---
+++ src/Classs.java
@@ -0,0 +1,40 @@
+public class Classs {
+    private int number;
+    private String word;
+    private int[] array;
+
+    public Classs(int number, String word, int[] array) {
+        this.number = number;
+        this.word = word;
+        this.array = array;
+    }
+
+    public Classs(){
+
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public int[] getArray() {
+        return array;
+    }
+
+    public void setArray(int[] array) {
+        this.array = array;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Classs classs = new Classs(18,"Kyrgyzstan",new int[]{1,2,3,4,5,6,7,8,9,10});
+        System.out.println("number: "+classs.getNumber()+"\nString: "+classs.getWord()+"\narray: "+ Arrays.toString(classs.getArray()));
+    }
+}   
