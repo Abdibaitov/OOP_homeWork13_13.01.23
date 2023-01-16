c2bac44ab99e7cc4a6330677129aabd131746f5a
Abdibaitov <91374399+Abdibaitov@users.noreply.github.com>
2023-01-16 18:34:46 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -6,4 +6,4 @@ public class Main {
         Classs classs = new Classs(18,"Kyrgyzstan",new int[]{1,2,3,4,5,6,7,8,9,10});
         System.out.println("number: "+classs.getNumber()+"\nString: "+classs.getWord()+"\narray: "+ Arrays.toString(classs.getArray()));
     }
-}   
+}
